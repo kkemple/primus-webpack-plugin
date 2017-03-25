3d790c41dc8b7926b6f489d71ead2d252fc93e94
Kurtis Kemple <kurtiskemple@gmail.com>
2017-03-25 14:33:29 -0400

    fix relative pathing issue

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
-const EventEmitter = require('events')
-const Primus = require('primus')
-const uglify = require('uglify-js')
+const EventEmitter = require('events');
+const Primus = require('primus');
+const uglify = require('uglify-js');
 
 class PrimusWebpackPlugin {
-  constructor (options) {
-    this.options = Object.assign({}, {
-      filename: 'primus-client.js',
-      minify: false,
-      primusOptions: {}
-    }, options)
+  constructor(options) {
+    this.options = Object.assign(
+      {},
+      {
+        filename: 'primus-client.js',
+        minify: false,
+        primusOptions: {},
+      },
+      options,
+    );
   }
 
-  apply (compiler) {
+  apply(compiler) {
     compiler.plugin('emit', (compilation, cb) => {
-      const primus = new Primus(new EventEmitter(), this.options.primusOptions)
-      const clientLib = primus.library()
-      const filename = this.options.filename.replace('[hash]', compilation.hash)
+      const primus = new Primus(new EventEmitter(), this.options.primusOptions);
+      const clientLib = primus.library();
+      const filename = this.options.filename.replace(
+        '[hash]',
+        compilation.hash,
+      );
       const source = this.options.minify
         ? uglify.minify(clientLib, { fromString: true })
-        : { code: clientLib }
+        : { code: clientLib };
 
       compilation.assets[filename] = {
-        source () {
-          return source.code
+        source() {
+          return source.code;
         },
-        size () {
-          return source.code.length
-        }
-      }
+        size() {
+          return source.code.length;
+        },
+      };
 
-      cb(null)
-    })
+      cb(null);
+    });
 
     // if HtmlWebpackPlugin is being utilized, add our script to file
-    compiler.plugin('compilation', (compilation) => {
-      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, cb) => {
-        const filename = this.options.filename.replace('[hash]', compilation.hash)
-        const scriptTag = `<script type="text/javascript" src="${filename}"></script>`
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin(
+        'html-webpack-plugin-before-html-processing',
+        (htmlPluginData, cb) => {
+          const filename = this.options.filename.replace(
+            '[hash]',
+            compilation.hash,
+          );
+          const scriptTag = `<script type="text/javascript" src="/${filename}"></script>`;
 
-        if (!htmlPluginData.plugin.options.inject || htmlPluginData.plugin.options.inject === 'head') {
-          htmlPluginData.html = htmlPluginData.html.replace('</head>', scriptTag + '</head>')
-        } else {
-          htmlPluginData.html = htmlPluginData.html.replace('</body>', scriptTag + '</body>')
-        }
+          if (
+            !htmlPluginData.plugin.options.inject ||
+            htmlPluginData.plugin.options.inject === 'head'
+          ) {
+            htmlPluginData.html = htmlPluginData.html.replace(
+              '</head>',
+              scriptTag + '</head>',
+            );
+          } else {
+            htmlPluginData.html = htmlPluginData.html.replace(
+              '</body>',
+              scriptTag + '</body>',
+            );
+          }
 
-        cb(null, htmlPluginData)
-      })
-    })
+          cb(null, htmlPluginData);
+        },
+      );
+    });
   }
 }
 
-module.exports = PrimusWebpackPlugin
+module.exports = PrimusWebpackPlugin;
